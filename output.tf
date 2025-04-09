8eb3afd6c86f66c2457a9780ac2a93671ae2fed0
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-09 11:25:46 +0200

    Reduced output clutter

--- output.tf
+++ output.tf
@@ -1,8 +1,11 @@
-output "github_pool" {
-  value = module.workload-identity.github_pool
+output "github_workload_identity_pool_name" {
+  value = module.workload-identity.github_pool.name
 }
 
-output "github_provider" {
-  value = module.workload-identity.github_provider
+output "github_workload_identity_pool_provider_id" {
+  value = module.workload-identity.github_provider.id
 }
 
+output "github_workload_identity_pool_provider_name" {
+  value = module.workload-identity.github_provider.name
+}
