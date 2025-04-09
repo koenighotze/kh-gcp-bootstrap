8794ab439061890ca5e3cf71e37cc0412a3a91bc
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-09 16:27:09 +0200

    Checkov setup

--- workload-identity-provider.tf
+++ workload-identity-provider.tf
@@ -2,6 +2,8 @@ data "github_user" "koenighotze" {
   username = var.github_username
 }
 
+#checkov:skip=CKV_GCP_118
+#checkov:skip=CKV_GCP_125
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider-${random_integer.rand.result}"
