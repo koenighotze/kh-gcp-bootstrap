f92b64e453674da9f4b1c011c7180d32341cc045
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-09 16:05:12 +0200

    fix workload fed for seed repository

--- seed-repository-setup.tf
+++ seed-repository-setup.tf
@@ -1,14 +1,21 @@
+locals {
+  roles = [
+    "roles/iam.workloadIdentityUser",
+    "roles/iam.serviceAccountTokenCreator"
+  ]
+}
+
 resource "github_actions_secret" "workload_identity_pool_name" {
   repository      = var.seed_repository_name
   secret_name     = "WORKLOAD_IDENTITY_PROVIDER_NAME"
   plaintext_value = google_iam_workload_identity_pool_provider.github_provider.name
 }
 
-resource "google_service_account_iam_binding" "workload_identity_binding" {
+resource "google_service_account_iam_binding" "workload_identity_bindings" {
+  for_each           = toset(local.roles)
   service_account_id = data.google_service_account.seed_service_account.id
-  role               = "roles/iam.workloadIdentityUser"
+  role               = each.value
   members = [
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.seed_repository_name}"
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.github_username}/${var.seed_repository_name}"
   ]
 }
-
