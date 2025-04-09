ee09d3bc222b7010fbba0926e75ac3527219331b
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-09 16:36:59 +0200

    Working on checkov findings

--- workload-identity-provider.tf
+++ workload-identity-provider.tf
@@ -2,15 +2,13 @@ data "github_user" "koenighotze" {
   username = var.github_username
 }
 
-#checkov:skip=CKV_GCP_118
-#checkov:skip=CKV_GCP_125
+#checkov:skip=CKV_GCP_118,CKV_GCP_125:Skip
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider-${random_integer.rand.result}"
   display_name                       = "GitHub Provider"
   description                        = "Provider for GitHub Actions"
 
-  # attribute.repository == "${var.seed_repository_name}"
   attribute_condition = <<EOT
     assertion.repository_owner_id == "${data.github_user.koenighotze.id}" && 
     assertion.ref == "refs/heads/main" &&          
