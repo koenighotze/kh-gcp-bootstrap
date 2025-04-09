4c7bb65a5300bb07fe26fa9fc4f628395a06042f
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-09 13:49:33 +0200

    add workload provider to repo

--- seed-repository-setup.tf
+++ seed-repository-setup.tf
@@ -1,7 +1,7 @@
 resource "github_actions_secret" "workload_identity_pool_name" {
   repository      = var.seed_repository_name
-  secret_name     = "WORKLOAD_IDENTITY_POOL_NAME"
-  plaintext_value = google_iam_workload_identity_pool.github_pool.name
+  secret_name     = "WORKLOAD_IDENTITY_PROVIDER_NAME"
+  plaintext_value = google_iam_workload_identity_pool_provider.github_provider.name
 }
 
 resource "google_service_account_iam_binding" "workload_identity_binding" {
