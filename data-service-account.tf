3e69229fb084322b46da087203963620778f24e3
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-28 11:32:16 +0200

    Seed SA is no longer a data

--- data-service-account.tf
+++
@@ -1,3 +0,0 @@
-# data "google_service_account" "seed_service_account" {
-#   account_id = var.seed_project_admin_service_account_id
-# }
--- output.tf
+++ output.tf
@@ -9,3 +9,7 @@ output "github_workload_identity_pool_provider_id" {
 output "github_workload_identity_pool_provider_name" {
   value = google_iam_workload_identity_pool_provider.github_provider.name
 }
+
+output "seed_service_account_email" {
+  value = google_service_account.seed_service_account.email
+}
